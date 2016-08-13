919ca625f049e1d41531608a9f94a42772c8712e
jingyongwang <lovedpotato@gmail.com>
2016-08-13 14:52:00 +0000

    week5 commit

--- src/About/AboutPage.jsx
+++ src/About/AboutPage.jsx
@@ -1,8 +1,22 @@
 export class AboutPage extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.changName = this.changName.bind(this);
+  }
+  
+  changName(){
+    browserHistory.push(`/about/${this.input.value}`);
+  }
+  
   render() {
     return (
       <div>
-        hi, I am ZeT.
+        <div>
+          hi, I am {this.props.params.name}
+        </div>
+        <input type = "text" ref = {(input) => this.input = input}  />
+        <button>Submit</button>
       </div>
     )
   }
--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ export class App extends React.Component {
       <div>
         React Example
         <div>
-          
+          <Link to  ="/counter">Counter</Link>
+          <Link to  ="/about/:name">about</Link>
         </div>
         {this.props.children}
       </div>
